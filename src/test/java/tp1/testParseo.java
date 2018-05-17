e4b43ea7ed9b1eb72f2e207a0f1c0c4bceccfbc2
AxelFulop <axelfulop@hotmail.com>
2018-05-16 21:51:41 -0300

    Se comparan todos los atributos del usuario
    corrección testeo parser entrega 0

--- src/test/java/tp1/testParseo.java
+++ src/test/java/tp1/testParseo.java
@@ -36,8 +36,29 @@ public class testParseo {
 	@Test
 	public void parseoCorrectoVariosClientes() throws IOException{
 		List<Cliente> clientes = parser.parsearClientes("src/main/resources/clientes.json");
+		// comparo nombres
 		Assert.assertEquals(cliente1JSON.getNombre(),clientes.get(0).getNombre());	
 		Assert.assertEquals(cliente2JSON.getNombre(),clientes.get(1).getNombre());
-
-	}	
+		//comparo apellidos
+		Assert.assertEquals(cliente1JSON.getApellido(),clientes.get(0).getApellido());	
+		Assert.assertEquals(cliente2JSON.getApellido(),clientes.get(1).getApellido());
+		//comparo tipo identificacion
+		Assert.assertEquals(cliente1JSON.getTipoIdentificacion(),clientes.get(0).getTipoIdentificacion());	
+		Assert.assertEquals(cliente2JSON.getTipoIdentificacion(),clientes.get(1).getTipoIdentificacion());
+		//comparo numero identificacion 
+		Assert.assertEquals(cliente1JSON.getNumId(),clientes.get(0).getNumId());	
+		Assert.assertEquals(cliente2JSON.getNumId(),clientes.get(1).getNumId());
+		//comparo Telefono
+		Assert.assertEquals(cliente1JSON.getTelefono(),clientes.get(0).getTelefono());	
+		Assert.assertEquals(cliente2JSON.getTelefono),clientes.get(1).getTelefono());
+		//comparo Direccion
+                Assert.assertEquals(cliente1JSON.getDireccion(),clientes.get(0).getDireccion());	
+		Assert.assertEquals(cliente2JSON.getDireccion(),clientes.get(1).getDireccion());
+		//comparo User
+		 Assert.assertEquals(cliente1JSON.getUser(),clientes.get(0).getUser());	
+		Assert.assertEquals(cliente2JSON.getUser(),clientes.get(1).getUser());
+		//comparo Contrasenia
+		 Assert.assertEquals(cliente1JSON.getContrasenia(),clientes.get(0).getContrasenia());	
+		Assert.assertEquals(cliente2JSON.getContrasenia(),clientes.get(1).getContrasenia());
+	}
 }
