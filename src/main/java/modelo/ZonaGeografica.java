64b5185a3f55ebe417e3f8e7c6076229938bdc66
nrmerlis <nicoxeneize.10@gmail.com>
2018-09-10 01:23:24 -0300

    Me olvide de commitear un archivo.

--- src/main/java/modelo/ZonaGeografica.java
+++ src/main/java/modelo/ZonaGeografica.java
@@ -1,32 +1,39 @@
 package modelo;
 
+import modelo.common.Tuple;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ZonaGeografica {
-public String nombre;
-public List<Transformador> transformadores = new ArrayList<Transformador>();
-
-public double getConsumoTotal() {
-	return transformadores.stream().mapToDouble(t->t.energiaQueEstaConsumiendo()).sum();
-}
-
-public String getNombre() {
-	return nombre;
-}
-
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-
-public List<Transformador> getTransformadores() {
-	return transformadores;
-}
-
-public void setTransformadores(List<Transformador> transformadores) {
-	this.transformadores = transformadores;
-}
+    public String nombre;
+    public List<Transformador> transformadores = new ArrayList<Transformador>();
+    public Double radioAbarcativo = 10D;
 
 
+    public double getConsumoTotal() {
+        return transformadores.stream().mapToDouble(t -> t.energiaQueEstaConsumiendo()).sum();
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Transformador> getTransformadores() {
+        return transformadores;
+    }
+
+    public void setTransformadores(List<Transformador> transformadores) {
+        this.transformadores = transformadores;
+    }
+
+
+
+    public Double getRadioAbarcativo() {
+        return radioAbarcativo;
+    }
 }
