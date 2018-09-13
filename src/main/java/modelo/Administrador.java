9c134333e84e2950e73ec8c31423f1b28d269fce
AxelFulop <axelfulop@hotmail.com>
2018-09-12 21:56:50 -0300

    el adm modifica los dispos

--- src/main/java/modelo/Administrador.java
+++ src/main/java/modelo/Administrador.java
@@ -4,6 +4,10 @@ package modelo;
 //import org.uqbar.commons.utils.Observable;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Date;
@@ -26,6 +30,40 @@ public class Administrador extends Usuario {
 		Date todayDate = new Date();
 		return (todayDate.getYear() - fechaAltaServicio.getYear()) *12 + abs(todayDate.getMonth()-fechaAltaServicio.getMonth());
 	}
+	
+	public void eliminarDispositivo (DispositivoInteligente disp)
+	{
+		EntityManagerFactory d = Persistence.createEntityManagerFactory("db");
+        EntityManager em = d.createEntityManager();
+        disp = em.find(DispositivoInteligente.class,disp.getId());
+        em.remove(disp);
+	
+	}
+	
+	public void CrearDispositivoInteligente (DispositivoInteligente disp)
+	{
+		
+		EntityManagerFactory d = Persistence.createEntityManagerFactory("db");
+        EntityManager em = d.createEntityManager();
+        disp = em.find(DispositivoInteligente.class,disp.getId());
+        em.persist(disp);
+	
+	}
+	
+	public void modificarDispositivo (DispositivoInteligente disp,String valor, String valorMod)
+	{
+		EntityManagerFactory d = Persistence.createEntityManagerFactory("db");
+        EntityManager em = d.createEntityManager();
+        int id = disp.getId();
+        disp = em.find(DispositivoInteligente.class,id);
+        em.createQuery( "update  DispositivoInteligente d set d  :valo = :valoMod  where d.id = :idd" )
+        .setParameter("valo",valor).setParameter("valoMod", valorMod).setParameter("idd", id)
+        .setMaxResults(10)
+        .getResultList();
+	
+	}
+	
+	
 	
 	
 }
