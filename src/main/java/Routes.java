fe6a5a0b07707d9698ae40e3f9b75c8f16c80e8a
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-10-11 10:35:00 -0300

    Agrego dependencias y creo varias clases con carpetas para empezar a usar spark. #8

---
+++ src/main/java/Routes.java
@@ -0,0 +1,16 @@
+import Servicios.JsonTransformer;
+
+import static spark.Spark.*;
+
+public class Routes {
+	private static final String API_CONTEXT = "/api/v1";
+
+
+	public Routes() {
+		setupEndpoints();
+	}
+
+	private void setupEndpoints() {
+		//Aca van las rutas
+	}
+}
