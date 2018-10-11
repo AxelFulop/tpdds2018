587d4ab181f5687fd6eff0bb0309dd67c8df3433
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-10-11 10:45:36 -0300

    Funcan rutas. #8

--- src/main/java/Routes.java
+++ src/main/java/Routes.java
@@ -1,9 +1,11 @@
 import Servicios.JsonTransformer;
 
+import java.io.File;
+
 import static spark.Spark.*;
 
 public class Routes {
-	private static final String API_CONTEXT = "/api/v1";
+	private static final String API_CONTEXT = "/";
 
 
 	public Routes() {
@@ -11,6 +13,9 @@ public class Routes {
 	}
 
 	private void setupEndpoints() {
-		//Aca van las rutas
-	}
+        get(API_CONTEXT +"/pepe", "application/json", (request, response)
+
+                -> new File("C:\\Users\\nicox\\Workspace\\2018-jm-group-10\\src\\main\\resources\\public\\index.html"), new JsonTransformer());
+
+    }
 }
