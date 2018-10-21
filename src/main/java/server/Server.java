e56337c6720267138ced7b179e1c95a3cdde1c4f
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-10-21 19:51:10 -0300

    El server o global, inicializa un usuario root para pruebas si no estas creado.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,12 +1,22 @@
 package server;
 
 
+import Servicios.UsuarioService;
+import modelo.Usuario;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
 	public class Server {
 		
 		public static void main(String[] args) {
+			if(UsuarioService.obtenerUsuario("root","root")==null)
+			{
+				Usuario root = new Usuario();
+				root.setContrasenia("root");
+				root.setNombreUsuario("root");
+				UsuarioService.persistir(root);
+			}
+
 			Spark.port(8080);
 			DebugScreen.enableDebugScreen();
 			
