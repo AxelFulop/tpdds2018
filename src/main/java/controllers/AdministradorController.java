17f1033699392432299f879eeb46cd48a55d77b4
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-10-23 01:59:18 -0300

    Mas cambios.

--- src/main/java/controllers/AdministradorController.java
+++ src/main/java/controllers/AdministradorController.java
@@ -15,12 +15,10 @@ public class AdministradorController {
 	public static ModelAndView home(Request req, Response res){
 		Long idAdministrador= Long.parseLong(req.params("id"));	
 		Usuario admin = UsuarioService.obtenerUsuarioPorId(idAdministrador);
-		
-	
 		//Administrador admin = new Administrador("root","root","Pedro","Fuentes",TipoIdentificacion.DNI,"123");
 		HashMap<String, Object> viewModel = new HashMap<>();
-		viewModel.put("nombre", admin.getNombre());
-		viewModel.put("apellido",admin.getApellido());
+		viewModel.put("name", admin.getNombre());
+		viewModel.put("id",admin.getId());
 		return new ModelAndView(viewModel,"adminDash.hbs");
 	}
 	public static ModelAndView dispositivo(Request req, Response res){
@@ -30,6 +28,8 @@ public class AdministradorController {
 		if(admin!=null)
 		{
 			HashMap<String, Object> viewModel = new HashMap<>();
+			viewModel.put("name",admin.getNombre());
+			viewModel.put("id",admin.getId());
 			return new ModelAndView(viewModel,"admin/crearDispositivo.hbs");
 		}
 		return null;
