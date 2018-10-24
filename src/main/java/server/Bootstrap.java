d4a7cc00cac620458bc069c7b021c999e1725aa9
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-10-24 19:23:13 -0300

    arreglo boostrap de login para usar password hasheada.

--- src/main/java/server/Bootstrap.java
+++ src/main/java/server/Bootstrap.java
@@ -3,6 +3,7 @@ package server;
 
 
 
+import Servicios.SHA256Builder;
 import modelo.Administrador;
 import modelo.Cliente;
 import modelo.TipoIdentificacion;
@@ -32,25 +33,14 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
 	
 	public void init(){
 	withTransaction(() ->{
-		clear();
 		
-		if((UsuarioService.obtenerUsuario("root","root")).equals(null)){
+		if((UsuarioService.obtenerUsuario("root",SHA256Builder.generarHash256("root"))).equals(null)){
 		Usuario root = new Usuario();
-		root.setContrasenia("root");
+		root.setContrasenia(SHA256Builder.generarHash256("root"));
 		root.setNombreUsuario("root");
 		UsuarioService.persistir(root);
 		
 		}
-		Cliente cliente1 = new Cliente("Antonio","Fondevila",TipoIdentificacion.DNI,"321",40539761,"Medrano 951","Toni","loco",0);
-		Cliente cliente2= new Cliente("Juan","Perez",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","JuanATR","qwerty",0);
-		Cliente cliente3= new Cliente("Lucas","Carrizo",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","luqui","asd",0);
-		if(UsuarioService.obtenerUsuario("JuanATR","qwerty").equals(null) && UsuarioService.obtenerUsuario("Toni","loco").equals(null)
-			&& UsuarioService.obtenerUsuario("luqui","asd").equals(null)){
-		UsuarioService.persistir(cliente1);
-		UsuarioService.persistir(cliente2);
-		UsuarioService.persistir(cliente3);
-		}
-		
 		});
 	
 	}
