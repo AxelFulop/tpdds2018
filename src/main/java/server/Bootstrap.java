b90c5677af588e2e61f5f303c86c5775fce1eb30
AxelFulop <axelfulop@hotmail.com>
2018-10-24 21:56:35 -0300

    tp del orto

--- src/main/java/server/Bootstrap.java
+++ src/main/java/server/Bootstrap.java
@@ -6,6 +6,9 @@ package server;
 import Servicios.SHA256Builder;
 import modelo.Administrador;
 import modelo.Cliente;
+import modelo.DispositivoEstandar;
+import modelo.DispositivoInteligente;
+import modelo.Estado;
 import modelo.TipoIdentificacion;
 import modelo.Usuario;
 
@@ -33,7 +36,15 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
 	
 	public void init(){
 	withTransaction(() ->{
-		
+		DispositivoInteligente tv = new DispositivoInteligente("tv", false,10d);
+		tv.persistir();
+		DispositivoInteligente aire = new DispositivoInteligente("aire",true,1d);
+		aire.setEstado(Estado.APAGADO);
+		aire.persistir();
+		Cliente cliente= new Cliente("test","test",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","pepe","pepe",0);
+		cliente.agregarDispositivoInteligente(tv);
+		cliente.agregarDispositivoInteligente(aire);
+		UsuarioService.persistir(cliente);
 		if((UsuarioService.obtenerUsuario("root",SHA256Builder.generarHash256("root"))).equals(null)){
 		Usuario root = new Usuario();
 		root.setContrasenia(SHA256Builder.generarHash256("root"));
