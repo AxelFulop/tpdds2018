1d0d9c64a2669cd76ba60c1827dfcee4ebd22e3d
AxelFulop <axelfulop@hotmail.com>
2018-10-24 22:31:10 -0300

    sas

--- src/main/java/server/Bootstrap.java
+++ src/main/java/server/Bootstrap.java
@@ -3,14 +3,22 @@ package server;
 
 
 
+import java.util.List;
+
+import Servicios.Parser;
 import Servicios.SHA256Builder;
+import modelo.Actuador;
 import modelo.Administrador;
 import modelo.Cliente;
 import modelo.DispositivoEstandar;
 import modelo.DispositivoInteligente;
 import modelo.Estado;
+import modelo.Optimizador;
+import modelo.Regla;
+import modelo.Sensor;
 import modelo.TipoIdentificacion;
 import modelo.Usuario;
+import modelo.sensores.SensorOptimizador;
 
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
@@ -36,15 +44,34 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
 	
 	public void init(){
 	withTransaction(() ->{
-		DispositivoInteligente tv = new DispositivoInteligente("tv", false,10d);
-		tv.persistir();
-		DispositivoInteligente aire = new DispositivoInteligente("aire",true,1d);
-		aire.setEstado(Estado.APAGADO);
-		aire.persistir();
-		Cliente cliente= new Cliente("test","test",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","pepe","pepe",0);
-		cliente.agregarDispositivoInteligente(tv);
-		cliente.agregarDispositivoInteligente(aire);
-		UsuarioService.persistir(cliente);
+		try {
+			Sensor sensorOptimizador = new SensorOptimizador();
+			Actuador actuadorOptimizador ;
+			Regla reglaApagadoOptimizacion;
+			Optimizador optimizador = new Optimizador();
+			
+			Cliente cliente1= new Cliente("Juan","Perez",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","lucas","asd",0);
+			Cliente cliente2= new Cliente("Juan","Perez",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","pedro","asd",0);
+			Cliente cliente3= new Cliente("Juan","Perez",TipoIdentificacion.DNI,"123",48262937,"Medrano 951","tito","asd",0);
+		
+			DispositivoInteligente aire = new DispositivoInteligente("aire",true,1d); 
+			DispositivoInteligente heladera = new DispositivoInteligente("heladera",true,1d); 
+			cliente1.agregarDispositivoInteligente(aire);
+			cliente1.agregarDispositivoInteligente(heladera);
+			cliente2.agregarDispositivoInteligente(aire);
+			cliente2.agregarDispositivoInteligente(heladera);
+			cliente3.agregarDispositivoInteligente(aire);
+			cliente3.agregarDispositivoInteligente(heladera);
+			aire.persistir();
+			heladera.persistir();
+			UsuarioService.persistir(cliente1);
+			UsuarioService.persistir(cliente2);
+			UsuarioService.persistir(cliente3);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if((UsuarioService.obtenerUsuario("root",SHA256Builder.generarHash256("root"))).equals(null)){
 		Usuario root = new Usuario();
 		root.setContrasenia(SHA256Builder.generarHash256("root"));
