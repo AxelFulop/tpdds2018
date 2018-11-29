dbbf330a031748292e18ab36950df767ba16965d
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-11-29 01:21:45 -0300

    Subo ultima vers.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -4,12 +4,19 @@ import spark.Spark;
 import spark.debug.DebugScreen;
 
 	public class Server {
-		
 		public static void main(String[] args) {
 			Bootstrap.main(null);
-			Spark.port(8080);
+			Spark.port(getHerokuAssignedPort());
 			DebugScreen.enableDebugScreen();
 			Router.configure(); 	
+		}
+
+		static int getHerokuAssignedPort() {
+			ProcessBuilder processBuilder = new ProcessBuilder();
+			if (processBuilder.environment().get("PORT") != null) {
+				return Integer.parseInt(processBuilder.environment().get("PORT"));
+			}
+			return 8080;
 		}
 
 	
