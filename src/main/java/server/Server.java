6787314f33504d930ed702fd88055d048011d6a6
nicoxeneize10 <nicoxeneize.10@gmail.com>
2018-12-04 00:43:16 -0300

    (Y).

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,18 +6,18 @@ import spark.debug.DebugScreen;
 	public class Server {
 		public static void main(String[] args) {
 			Bootstrap.main(null);
-			Spark.port(8080);
+			Spark.port(getHerokuAssignedPort());
 			DebugScreen.enableDebugScreen();
 			Router.configure(); 	
 		}
-/*
+
 		static int getHerokuAssignedPort() {
 			ProcessBuilder processBuilder = new ProcessBuilder();
 			if (processBuilder.environment().get("PORT") != null) {
 				return Integer.parseInt(processBuilder.environment().get("PORT"));
 			}
 			return 8080;
-		}*/
+		}
 
 	
 }
